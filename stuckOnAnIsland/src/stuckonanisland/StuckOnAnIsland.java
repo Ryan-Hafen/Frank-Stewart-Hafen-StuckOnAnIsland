e4567d6aec73709ef9295820d77e92f3100f65b0
Ryan-Hafen <ryan.hafen@icloud.com>
2017-05-23 21:19:21 -0700

    fixing error is classes from renaming

---
+++ stuckOnAnIsland/src/stuckonanisland/StuckOnAnIsland.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stuckonanisland;
+
+import byui.cit260.stuckOnAnIsland.model.Actor;
+import byui.cit260.stuckOnAnIsland.model.Backpack;
+import byui.cit260.stuckOnAnIsland.model.Game;
+import byui.cit260.stuckOnAnIsland.model.Player;
+import byui.cit260.stuckOnAnIsland.model.IslandInventory;
+import byui.cit260.stuckOnAnIsland.model.RaftMaterial;
+import byui.cit260.stuckOnAnIsland.model.RaftStatus;
+import byui.cit260.stuckOnAnIsland.model.ToolInventory;
+import byui.cit260.stuckOnAnIsland.model.WreckageInventory;
+
+/**
+ *
+ * @author RyanHafen
+ */
+public class StuckOnAnIsland {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Actor Class
+        Actor actorOne = new Actor();
+        
+        actorOne.setActorName("George");
+        actorOne.getCurrentCoordinate(3, 1);
+        actorOne.getCurrentHealth();
+        
+        String actorInfo = actorOne.toString();
+        System.out.println(actorInfo);
+        
+        //Backpack Class
+        Backpack backpack = new Backpack();
+        
+        backpack.setResourceCapacity(100);
+        backpack.setResourceQuantity(3);
+        backpack.setResourceDescription("apple");
+        
+        String backpackInfo = backpack.toString();
+        System.out.println(backpackInfo);
+        
+        //Game Class
+        Game gameOne = new Game();
+        
+        gameOne.setNumberRounds(5);
+        gameOne.setTotalTime(70000);
+        
+        String gameInfo = gameOne.toString();
+        System.out.println(gameInfo);
+        
+        //Player Class
+        Player playerOne = new Player();
+        
+        playerOne.setName("Ryan");
+        playerOne.setBesTime(7.00);
+        
+        String playerInfo = playerOne.toString();
+        System.out.println(playerInfo);
+        
+        //islandInventory Class
+        IslandInventory islandInventory = new IslandInventory();
+        
+        islandInventory.setDescription("water");
+        islandInventory.setInventoryQuantity(1);
+        islandInventory.setInventorySize(25);
+        islandInventory.setHealthPoints(5.5);
+        
+        String islandInventoryInfo = islandInventory.toString();
+        System.out.println(islandInventoryInfo);
+        
+        //raftMaterial Class
+        RaftMaterial raftMaterial = new RaftMaterial();
+        
+        raftMaterial.setMaterialVine("vine");
+        raftMaterial.setMaterialWood("wood");
+        
+        String raftMaterialInfo = raftMaterial.toString();
+        System.out.println(raftMaterialInfo);
+        
+        //raftStatus Class
+        RaftStatus raftStatus = new RaftStatus();
+        
+        raftStatus.setDescription("raft");
+        raftStatus.setRaftCompletionPercentage(95.6);
+        raftStatus.setRaftMaterialList("wood");
+        
+        String raftStatusInfo = raftStatus.toString();
+        System.out.println(raftStatusInfo);
+        
+        //toolInventory Class
+        ToolInventory toolInventory = new ToolInventory();
+        
+        toolInventory.setDescription("axe");
+        toolInventory.setToolType("weapon");
+        
+        String toolInventoryInfo = toolInventory.toString();
+        System.out.println(toolInventoryInfo);
+        
+        //wreckageInventory Class
+        WreckageInventory wreckageInventory = new WreckageInventory();
+        
+        wreckageInventory.setDescription("matches");
+        wreckageInventory.setInventoryType("fire");
+        
+        String wreckageInventoryInfo = wreckageInventory.toString();
+        System.out.println(wreckageInventoryInfo);
+    }
+    
+}
