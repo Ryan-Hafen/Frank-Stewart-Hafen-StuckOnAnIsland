80634bbaadda83c83270d64ae22629a5167b1cab
Ryan-Hafen <ryan.hafen@icloud.com>
2017-05-23 18:45:53 -0700

    Team Assignment Create Control and test case

--- stuckOnAnIsland/src/byui/cit260/stuckOnAnIsland/control/BuildControl.java
+++ stuckOnAnIsland/src/byui/cit260/stuckOnAnIsland/control/BuildControl.java
@@ -18,6 +18,18 @@ public class BuildControl {
         if (amountRequired < 0 || amountRequired > 100) {
                 return -1;
         }
+        double woodNeeded = amountRequired - amountInInventory;
+                
+        return woodNeeded;
+    }
+    
+    public double calcVineNeeded(double amountInInventory, double amountRequired) {
+        if (amountInInventory < 0 || amountInInventory > 100) {
+                return -1;
+        }
+        if (amountRequired < 0 || amountRequired > 100) {
+                return -1;
+        }
         double vineNeeded = amountRequired - amountInInventory;
                 
         return vineNeeded;
