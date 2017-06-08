804a61c66904a5e96eaa97a9c71affb3011b3451
Ryan-Hafen <ryan.hafen@icloud.com>
2017-06-07 19:04:28 -0700

    edit help menu to follow end user story

--- stuckOnAnIsland/src/stuckonanisland/StuckOnAnIsland.java
+++ stuckOnAnIsland/src/stuckonanisland/StuckOnAnIsland.java
@@ -51,91 +51,91 @@ public class StuckOnAnIsland {
         startProgramView.displayStartProgramView();
         
         
-        //Actor Class
-        Actor actorOne = new Actor();
-        
-        actorOne.setActorName("George");
-        actorOne.getCurrentCoordinate(3, 1);
-        actorOne.getCurrentHealth();
-        
-        String actorInfo = actorOne.toString();
-        System.out.println(actorInfo);
-        
-        //Backpack Class
-        Backpack backpack = new Backpack();
-        
-        backpack.setResourceCapacity(100);
-        backpack.setResourceQuantity(3);
-        backpack.setResourceDescription("apple");
-        
-        String backpackInfo = backpack.toString();
-        System.out.println(backpackInfo);
-        
-        //Game Class
-        Game gameOne = new Game();
-        
-        gameOne.setNumberRounds(5);
-        gameOne.setTotalTime(70000);
-        
-        String gameInfo = gameOne.toString();
-        System.out.println(gameInfo);
-        
-        //Player Class
-        Player playerOne = new Player();
-        
-        playerOne.setName("Ryan");
-        playerOne.setBesTime(7.00);
-        
-        String playerInfo = playerOne.toString();
-        System.out.println(playerInfo);
-        
-        //islandInventory Class
-        IslandInventory islandInventory = new IslandInventory();
-        
-        islandInventory.setDescription("water");
-        islandInventory.setInventoryQuantity(1);
-        islandInventory.setInventorySize(25);
-        islandInventory.setHealthPoints(5.5);
-        
-        String islandInventoryInfo = islandInventory.toString();
-        System.out.println(islandInventoryInfo);
-        
-        //raftMaterial Class
-        RaftMaterial raftMaterial = new RaftMaterial();
-        
-        raftMaterial.setMaterialVine("vine");
-        raftMaterial.setMaterialWood("wood");
-        
-        String raftMaterialInfo = raftMaterial.toString();
-        System.out.println(raftMaterialInfo);
-        
-        //raftStatus Class
-        RaftStatus raftStatus = new RaftStatus();
-        
-        raftStatus.setDescription("raft");
-        raftStatus.setRaftCompletionPercentage(95.6);
-        raftStatus.setRaftMaterialList("wood");
-        
-        String raftStatusInfo = raftStatus.toString();
-        System.out.println(raftStatusInfo);
-        
-        //toolInventory Class
-        ToolInventory toolInventory = new ToolInventory();
-        
-        toolInventory.setDescription("axe");
-        toolInventory.setToolType("weapon");
-        
-        String toolInventoryInfo = toolInventory.toString();
-        System.out.println(toolInventoryInfo);
-        
-        //wreckageInventory Class
-        WreckageInventory wreckageInventory = new WreckageInventory();
-        
-        wreckageInventory.setDescription("matches");
-        wreckageInventory.setInventoryType("fire");
-        
-        String wreckageInventoryInfo = wreckageInventory.toString();
-        System.out.println(wreckageInventoryInfo);
+//        //Actor Class
+//        Actor actorOne = new Actor();
+//        
+//        actorOne.setActorName("George");
+//        actorOne.getCurrentCoordinate(3, 1);
+//        actorOne.getCurrentHealth();
+//        
+//        String actorInfo = actorOne.toString();
+//        System.out.println(actorInfo);
+//        
+//        //Backpack Class
+//        Backpack backpack = new Backpack();
+//        
+//        backpack.setResourceCapacity(100);
+//        backpack.setResourceQuantity(3);
+//        backpack.setResourceDescription("apple");
+//        
+//        String backpackInfo = backpack.toString();
+//        System.out.println(backpackInfo);
+//        
+//        //Game Class
+//        Game gameOne = new Game();
+//        
+//        gameOne.setNumberRounds(5);
+//        gameOne.setTotalTime(70000);
+//        
+//        String gameInfo = gameOne.toString();
+//        System.out.println(gameInfo);
+//        
+//        //Player Class
+//        Player playerOne = new Player();
+//        
+//        playerOne.setName("Ryan");
+//        playerOne.setBesTime(7.00);
+//        
+//        String playerInfo = playerOne.toString();
+//        System.out.println(playerInfo);
+//        
+//        //islandInventory Class
+//        IslandInventory islandInventory = new IslandInventory();
+//        
+//        islandInventory.setDescription("water");
+//        islandInventory.setInventoryQuantity(1);
+//        islandInventory.setInventorySize(25);
+//        islandInventory.setHealthPoints(5.5);
+//        
+//        String islandInventoryInfo = islandInventory.toString();
+//        System.out.println(islandInventoryInfo);
+//        
+//        //raftMaterial Class
+//        RaftMaterial raftMaterial = new RaftMaterial();
+//        
+//        raftMaterial.setMaterialVine("vine");
+//        raftMaterial.setMaterialWood("wood");
+//        
+//        String raftMaterialInfo = raftMaterial.toString();
+//        System.out.println(raftMaterialInfo);
+//        
+//        //raftStatus Class
+//        RaftStatus raftStatus = new RaftStatus();
+//        
+//        raftStatus.setDescription("raft");
+//        raftStatus.setRaftCompletionPercentage(95.6);
+//        raftStatus.setRaftMaterialList("wood");
+//        
+//        String raftStatusInfo = raftStatus.toString();
+//        System.out.println(raftStatusInfo);
+//        
+//        //toolInventory Class
+//        ToolInventory toolInventory = new ToolInventory();
+//        
+//        toolInventory.setDescription("axe");
+//        toolInventory.setToolType("weapon");
+//        
+//        String toolInventoryInfo = toolInventory.toString();
+//        System.out.println(toolInventoryInfo);
+//        
+//        //wreckageInventory Class
+//        WreckageInventory wreckageInventory = new WreckageInventory();
+//        
+//        wreckageInventory.setDescription("matches");
+//        wreckageInventory.setInventoryType("fire");
+//        
+//        String wreckageInventoryInfo = wreckageInventory.toString();
+//        System.out.println(wreckageInventoryInfo);
     }
     
 }
