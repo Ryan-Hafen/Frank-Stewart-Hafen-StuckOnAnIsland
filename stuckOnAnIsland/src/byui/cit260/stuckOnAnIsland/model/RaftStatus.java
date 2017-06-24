c2bf9157c2e3c9c5f883aa76b5f309784f75283e
Ryan-Hafen <ryan.hafen@icloud.com>
2017-06-23 21:26:40 -0700

    created game enum and lists

--- stuckOnAnIsland/src/byui/cit260/stuckOnAnIsland/model/RaftStatus.java
+++ stuckOnAnIsland/src/byui/cit260/stuckOnAnIsland/model/RaftStatus.java
@@ -14,70 +14,17 @@ import java.util.Objects;
 public class RaftStatus implements Serializable{
     private String description;
     private double raftCompletionPercentage;
-    private String raftMaterialList;
+    private int woodRequired;
+    private int vineRequired;
+    private int woodGathered;
+    private int vineGathered;
 
     public RaftStatus() {
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getRaftCompletionPercentage() {
-        return raftCompletionPercentage;
-    }
-
-    public void setRaftCompletionPercentage(double raftCompletionPercentage) {
-        this.raftCompletionPercentage = raftCompletionPercentage;
-    }
-
-    public String getRaftMaterialList() {
-        return raftMaterialList;
-    }
-
-    public void setRaftMaterialList(String raftMaterialList) {
-        this.raftMaterialList = raftMaterialList;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 3;
-        hash = 43 * hash + Objects.hashCode(this.description);
-        hash = 43 * hash + (int) (Double.doubleToLongBits(this.raftCompletionPercentage) ^ (Double.doubleToLongBits(this.raftCompletionPercentage) >>> 32));
-        hash = 43 * hash + Objects.hashCode(this.raftMaterialList);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final RaftStatus other = (RaftStatus) obj;
-        if (Double.doubleToLongBits(this.raftCompletionPercentage) != Double.doubleToLongBits(other.raftCompletionPercentage)) {
-            return false;
-        }
-        if (!Objects.equals(this.description, other.description)) {
-            return false;
-        }
-        if (!Objects.equals(this.raftMaterialList, other.raftMaterialList)) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "RaftStatus{" + "description=" + description + ", raftCompletionPercentage=" + raftCompletionPercentage + ", raftMaterialList=" + raftMaterialList + '}';
+        this.description = "Build a raft to escape.";
+        this.raftCompletionPercentage = 0.0;
+        this.woodRequired = 100;
+        this.vineRequired = 100;
+        this.woodGathered = 0;
+        this.vineGathered = 0;
     }
 }
