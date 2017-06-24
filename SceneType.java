fffb9e6a2dfeab61c2347c6e64d02f4efb674a25
Stewart-John <ste74002@byui.edu>
2017-06-23 22:30:29 -0600

    Add files via upload

---
+++ SceneType.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260homework;
+
+/**
+ *
+ * @author germa_000
+ */
+public enum SceneType {
+    reef,
+    beacha,
+    beachb,
+    oceana,
+    oceanb,
+    bamboa,
+    bambob,
+    cave,
+    oceanc,
+    jungle,
+    mountain,
+    cliff,
+    oceand,
+    raft,
+    lagoon,
+    waterfall,
+    beachc,
+    camp,
+    bamboc,
+    beachd,
+    wreckage;
+}
